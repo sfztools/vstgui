104172476111a255a87d9853a4b6502071130b9d
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-09-11 14:01:24 +0200

    New X11 filedialog: security fixes, works in Ardour6 bundle

--- vstgui/lib/platform/linux/x11fileselector.cpp
+++ vstgui/lib/platform/linux/x11fileselector.cpp
@@ -4,13 +4,18 @@
 
 #include "../../cfileselector.h"
 #include <unistd.h>
+#include <spawn.h>
+#include <sys/wait.h>
 #include <string>
+#include <vector>
+#include <memory>
+#include <cerrno>
+extern "C" { extern char **environ; }
 
 //------------------------------------------------------------------------
 namespace VSTGUI {
 namespace X11 {
 
-static constexpr auto kdialogpath = "/usr/bin/kdialog";
 static constexpr auto zenitypath = "/usr/bin/zenity";
 
 //------------------------------------------------------------------------
@@ -18,7 +23,6 @@ struct FileSelector : CNewFileSelector
 {
 	FileSelector (CFrame* parent, Style style) : CNewFileSelector (parent), style (style)
 	{
-		identifiyExDialogType ();
 	}
 
 	~FileSelector ()
@@ -29,16 +33,7 @@ struct FileSelector : CNewFileSelector
 	bool runInternal (CBaseObject* delegate) override
 	{
 		this->delegate = delegate;
-		switch (exDialogType)
-		{
-			case ExDialogType::kdialog:
-				return runKDialog ();
-			case ExDialogType::zenity:
-				return runZenity ();
-			case ExDialogType::none:
-				break;
-		}
-		return false;
+		return runZenity ();
 	}
 
 	void cancelInternal () override { closeProcess (); }
@@ -47,23 +42,25 @@ struct FileSelector : CNewFileSelector
 	{
 		if (runInternal (nullptr))
 		{
-			while (feof (pipe) == 0)
+			std::string path;
+			path.reserve(1024);
+
+			ssize_t count;
+			char buffer[1024];
+			while ((count = read (readerFd, buffer, sizeof (buffer))) > 0 ||
+				   (count == -1 && errno == EINTR))
 			{
-				char* line = nullptr;
-				size_t count = 0;
-				if (getline (&line, &count, pipe) < 0)
+				if (count > 0)
+					path.append (buffer, count);
+			}
+
+			if (count != -1)
+			{
+				if (! path.empty () && path[0] == '/')
 				{
-					break;
-				}
-				if (line)
-				{
-					if (line[0] == '/')
-					{
-						std::string path (line);
-						path.erase (path.size () - 1);
-						result.emplace_back (path);
-					}
-					free (line);
+					if (path.back () == '\n')
+						path.pop_back ();
+					result.emplace_back (path);
 				}
 			}
 		}
@@ -71,86 +68,143 @@ struct FileSelector : CNewFileSelector
 	}
 
 private:
-	enum class ExDialogType
-	{
-		none,
-		kdialog,
-		zenity
-	};
-
-	void identifiyExDialogType ()
-	{
-		if (auto file = fopen (kdialogpath, "r"))
-		{
-			fclose (file);
-			exDialogType = ExDialogType::kdialog;
-		}
-		else if (auto file = fopen (zenitypath, "r"))
-		{
-			fclose (file);
-			exDialogType = ExDialogType::zenity;
-		}
-	}
-
-	bool runKDialog ()
-	{
-		std::string command = kdialogpath;
-		command += " ";
-		if (style == Style::kSelectFile)
-			command += "--getopenfilename --separate-output";
-		else if (style == Style::kSelectSaveFile)
-			command += "--getsavefilename";
-		else if (style == Style::kSelectDirectory)
-			command += "--getexistingdirectory";
-        if (allowMultiFileSelection)
-            command += " --multiple";
-		if (!title.empty ())
-			command += " --title '" + title.getString () + "'";
-        if (!initialPath.empty ())
-            command += " \"" + initialPath.getString () + "\"";
-		if (startProcess (command.data ()))
-		{
-			return true;
-		}
-		return false;
-	}
-
 	bool runZenity ()
 	{
-		std::string command = zenitypath;
-		command += " --file-selection ";
-		if (style == Style::kSelectDirectory)
-			command += "--directory";
-		else if (style == Style::kSelectSaveFile)
-			command += "--save --confirm-overwrite";
-		if (!title.empty ())
-			command += "--title=\"" + title.getString () + "\"";
-		if (!initialPath.empty ())
-			command += "--filename=\"" + initialPath.getString () + "\"";
+		std::vector<std::string> args;
 
-		if (startProcess (command.data ()))
+		args.push_back (zenitypath);
+		args.push_back ("--file-selection");
+
+		if (style == Style::kSelectDirectory)
+			args.push_back ("--directory");
+		else if (style == Style::kSelectSaveFile)
+		{
+			args.push_back ("--save");
+			args.push_back ("--confirm-overwrite");
+		}
+		if (!title.empty ())
+			args.push_back ("--title=" + title.getString ());
+		if (!initialPath.empty ())
+			args.push_back ("--filename=" + initialPath.getString ());
+
+		std::vector<char*> argv (args.size () + 1);
+		for (size_t i = 0, n = args.size (); i < n; ++i)
+			argv[i] = const_cast<char*>(args[i].c_str ());
+
+		if (startProcess (argv.data()))
 		{
 			return true;
 		}
 		return false;
 	}
 
-	bool startProcess (const char* command)
+	bool startProcess (char* argv[])
 	{
-		pipe = popen (command, "re");
-		return pipe != nullptr;
+		closeProcess();
+
+		SpawnAttrPtr attr { new posix_spawnattr_t };
+		if (posix_spawnattr_init (attr.get ()) != 0)
+		{
+			delete attr.release ();
+			return false;
+		}
+
+		SpawnActionsPtr actions { new posix_spawn_file_actions_t };
+		if (posix_spawn_file_actions_init (actions.get ()) != 0)
+		{
+			delete actions.release ();
+			return false;
+		}
+
+		struct PipePair
+		{
+			int fd[2] = { -1, -1 };
+			~PipePair ()
+			{
+				if (fd[0] != -1) close (fd[0]);
+				if (fd[1] != -1) close (fd[1]);
+			}
+		};
+
+		PipePair rw;
+		if (pipe (rw.fd) != 0)
+			return false;
+
+#if 0
+		char** envp = environ;
+#else
+		std::vector<char*> cleanEnviron;
+		cleanEnviron.reserve (256);
+		for (char** envp = environ; *envp; ++envp)
+		{
+			// ensure the process will link with system libraries,
+			// and not these from the Ardour bundle.
+			if (strncmp (*envp, "LD_LIBRARY_PATH=", 16) == 0)
+				continue;
+			cleanEnviron.push_back (*envp);
+		}
+		cleanEnviron.push_back(nullptr);
+		char** envp = cleanEnviron.data ();
+#endif
+
+		if (posix_spawnattr_setflags(attr.get (), POSIX_SPAWN_USEVFORK) != 0 ||
+			posix_spawn_file_actions_adddup2 (actions.get (), rw.fd[1], STDOUT_FILENO) != 0)
+		{
+			return false;
+		}
+
+		if (posix_spawn(&spawnPid, zenitypath, actions.get (), attr.get (), argv, envp) != 0)
+			return false;
+
+		close (rw.fd[1]);
+		rw.fd[1] = -1;
+		readerFd = rw.fd[0];
+		rw.fd[0] = -1;
+
+		return true;
 	}
 
 	void closeProcess ()
 	{
-		if (pipe)
-			pclose (pipe);
-		pipe = nullptr;
+		if (spawnPid != -1)
+		{
+			if (waitpid (spawnPid, nullptr, WNOHANG) == 0)
+			{
+				kill (spawnPid, SIGTERM);
+				waitpid (spawnPid, nullptr, 0);
+			}
+			spawnPid = -1;
+		}
+
+		if (readerFd != -1)
+		{
+			close (readerFd);
+			readerFd = -1;
+		}
 	}
+
+	pid_t spawnPid = -1;
+	int readerFd = -1;
+
 	Style style;
 	SharedPointer<CBaseObject> delegate;
-	ExDialogType exDialogType{ExDialogType::none};
-	FILE* pipe{nullptr};
+
+	struct PosixSpawnDeleter {
+	public:
+		void operator()(posix_spawnattr_t* attr)
+		{
+			posix_spawnattr_destroy(attr);
+			delete attr;
+		}
+		void operator()(posix_spawn_file_actions_t* actions)
+		{
+			posix_spawn_file_actions_destroy(actions);
+			delete actions;
+		}
+	};
+
+	typedef std::unique_ptr<posix_spawnattr_t, PosixSpawnDeleter> SpawnAttrPtr;
+	typedef std::unique_ptr<posix_spawn_file_actions_t, PosixSpawnDeleter> SpawnActionsPtr;
 };
 
 //------------------------------------------------------------------------
